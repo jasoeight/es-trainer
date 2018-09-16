fb2dcd9765a913d44e0f23ee2b136d30aa0f20b6
Jan Soehnitz <jan.soehnitz@googlemail.com>
2018-09-16 10:48:58 +0200

    initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,53 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
-}
+    root: true,
+
+    env: {
+        node: true
+    },
+
+    'extends': [
+        'plugin:vue/essential',
+        '@vue/standard'
+    ],
+
+    rules: {
+        'no-console': 'off',
+        'no-debugger': 'off',
+        'graphql/template-strings': [
+            'error',
+            {
+                env: 'literal',
+                projectName: 'app'
+            }
+        ],
+        'no-template-curly-in-string': 'off',
+        'arrow-parens': 0,
+        'generator-star-spacing': 0,
+        indent: [
+            'error',
+            4,
+            {
+                SwitchCase: 1
+            }
+        ],
+        'no-new': 0,
+        'comma-dangle': 0,
+        'no-bitwise': 0,
+        'no-plusplus': 0,
+        'space-before-function-paren': 0,
+        semi: [
+            'error',
+            'always'
+        ],
+        'prefer-promise-reject-errors': 0,
+        'no-unused-expressions': 0
+    },
+
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+
+    plugins: [
+        'graphql'
+    ]
+};
--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
+import '@babel/polyfill';
+import Vue from 'vue';
+import './plugins/vuetify';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import './registerServiceWorker';
+import { createProvider } from './vue-apollo';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    apolloProvider: createProvider(),
+    render: h => h(App)
+}).$mount('#app');
