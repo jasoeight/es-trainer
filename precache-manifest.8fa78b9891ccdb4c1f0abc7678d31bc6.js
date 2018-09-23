d3de76023ddcd6351bf7e9acde5b64f1129ca846
Jan Soehnitz <jan.soehnitz@googlemail.com>
2018-09-23 16:44:02 +0200

    Updates

---
+++ precache-manifest.8fa78b9891ccdb4c1f0abc7678d31bc6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc1ce3004a1121e37f60",
+    "url": "/js/error404.924fe6ee.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d26bdf312faafcf6b792",
+    "url": "/js/translate.d209f651.js"
+  },
+  {
+    "revision": "f6909b1538515d9a048f",
+    "url": "/js/practice.aecf2495.js"
+  },
+  {
+    "revision": "ba0963ee80c344c13cdd",
+    "url": "/js/new.8af99b82.js"
+  },
+  {
+    "revision": "9db1f03939d34a81bcc6",
+    "url": "/js/error403.2c1603a5.js"
+  },
+  {
+    "revision": "7851d0cb6eccf01686fa",
+    "url": "/js/chunk-vendors.f6de9b04.js"
+  },
+  {
+    "revision": "d840d91736e49ee1ce58",
+    "url": "/js/chunk-2d2160d1.4ce2eeb1.js"
+  },
+  {
+    "revision": "baf97a150dbe00b811de",
+    "url": "/js/app.64af6185.js"
+  },
+  {
+    "revision": "a169d78606c1b5c98f71cbcee6296f27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7851d0cb6eccf01686fa",
+    "url": "/css/chunk-vendors.a62af960.css"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5bdc65127deb46e7fbd9005dac95c161.js"
+  "/precache-manifest.8fa78b9891ccdb4c1f0abc7678d31bc6.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "es-trainer"});
